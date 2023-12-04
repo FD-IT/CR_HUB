dcb65585cbe9b9ce5bf326cb14df85917cba4381
Stefan Ilic <ilicstefan272@gmail.com>
2023-12-04 18:27:33 +0100

    Dodat niz objekata za partnere
    Dodato je ukupno 5 partnera

---
+++ models/partneri.js
@@ -0,0 +1,32 @@
+let partneri = [
+  {
+    ID: 1,
+    nazivKompanije: "Zlatiborac",
+    kontaktOsoba: "Stefan Ilic",
+    adresa: "Adresa kompanije 1",
+  },
+  {
+    ID: 2,
+    nazivKompanije: "SoccerBet",
+    kontaktOsoba: "Marko Markovic",
+    adresa: "Adresa kompanije 2",
+  },
+  {
+    ID: 3,
+    nazivKompanije: "Samsung",
+    kontaktOsoba: "Srdjan Ilic",
+    adresa: "Adresa kompanije 3",
+  },
+  {
+    ID: 4,
+    nazivKompanije: "Apple",
+    kontaktOsoba: "Jovana Filipovic",
+    adresa: "Adresa kompanije 4",
+  },
+  {
+    ID: 5,
+    nazivKompanije: "Nokia",
+    kontaktOsoba: "Marko Tasic",
+    adresa: "Adresa kompanije 5",
+  },
+];
