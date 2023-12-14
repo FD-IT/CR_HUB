318bda8b2914e863021aa4fa208214a4fac03d6c
Stefan Ilic <ilicstefan272@gmail.com>
2023-12-14 16:05:38 +0100

    Dodavanje server.js i instalacija Express.js zajedno sa nodemon

--- models/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-app.listen(3000);
-
-app.get("/", (req, res) => {
-  res.send("Dobrodosli na CR_Hub");
-  console.log("Server je uspesno pokrenut!");
-});
