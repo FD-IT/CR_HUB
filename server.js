ea713a492704955e153d8d0a0c075574b1d4b048
BoranBubanja <152922090+BoranBubanja@users.noreply.github.com>
2023-12-15 02:39:47 +0100

    importovan je ruter u server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const app = express();
 app.listen(3000);
 
@@ -6,3 +7,7 @@ app.get("/", (req, res) => {
   res.send("Dobrodosli na CR_Hub");
   console.log("Server je uspesno pokrenut!");
 });
+
+const partneriRouter = require("./routes/partneriRuter");
+
+app.use("/users",partneriRouter);
