a49edc34df8d7127a784467fe4694bc5bbe288c1
jovanaantic <anticjovana93@gmail.com>
2023-12-16 19:53:53 +0100

    funkcija koja prihvata post zahtev na putanji /partneri

--- routes/partneri.js
+++ routes/partneri.js
@@ -8,4 +8,10 @@ router.get("/", (req, res) => {
     console.log("Zahtev za prikazivanje svih partnera je primljen!");
   });
 
+// POST /partneri
+router.post("/", (req, res) => {
+    res.send("Kreiraj novog partnera")
+});
+
 module.exports = router;
+
