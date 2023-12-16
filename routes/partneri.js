76a33ce99f3cae2035983eb501cf41e3035aabae
Nikola <nikolastanojevic252@gmail.com>
2023-12-16 23:37:55 +0100

    Patch na putanji /partneri/:id

--- routes/partneri.js
+++ routes/partneri.js
@@ -13,5 +13,12 @@ router.post("/", (req, res) => {
     res.send("Kreiraj novog partnera")
 });
 
+// PATCH /partneri/:id
+router.patch("/:id", (req, res) => {
+  const partnerId = req.params.id;
+  res.send(`Izmeni partnera sa datim ID: ${partnerId}`);
+  console.log(`Zahtev za izmenu partnera ${partnerId} je primljen!`);
+});
+
 module.exports = router;
 
