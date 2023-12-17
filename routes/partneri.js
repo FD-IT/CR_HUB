6214615c12829ef5664b8809bf7e045d93140a94
markovicm668 <markovicm668@gmail.com>
2023-12-17 17:49:26 +0100

    Update partneri.js

--- routes/partneri.js
+++ routes/partneri.js
@@ -13,10 +13,24 @@ router.post("/", (req, res) => {
     res.send("Kreiraj novog partnera")
 });
 
+// PATCH /partneri/:id
+router.patch("/:id", (req, res) => {
+  const partnerId = req.params.id;
+  res.send(`Izmeni partnera sa datim ID: ${partnerId}`);
+  console.log(`Zahtev za izmenu partnera ${partnerId} je primljen!`);
+});
+
 // DELETE /partneri
 router.delete("/:id", (req, res) => {
   res.send("Obrisi partnera sa ID-jem ${req.params.id}");
 });
+
+// GET partner by ID
+router.get("/:id", (req,res) => {
+
+  const partnerId = req.params.id;
+   res.send('Prikazi partnera sa datim ID: ${partnerId}');
+})
 
 module.exports = router;
 
