eed89de72af01360601672c16056d71069ebac5a
markovicm668 <markovicm668@gmail.com>
2023-12-22 16:36:29 +0100

    reseno

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-app.listen(3000);
+// postavljen view engine, ukljuciti kada frontend isprati
+// app.set('view engine', 'ejs');
+
+//za koriscenje putanja
+app.use(express.urlencoded({extended: true}));
+
+// adresa DB
+const uri = "mongodb+srv://marko:marko@crhub.ds7emhj.mongodb.net/?retryWrites=true&w=majority";
+
+// connect to db
+mongoose.connect(uri)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(3000, () => {
+      console.log('listening for requests on port', 3000)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
+
+// ukljuceno u funkciji za povezivanje na DB
+// app.listen(3000);
 
 app.get("/", (req, res) => {
   res.send("Dobrodosli na CR_Hub");
