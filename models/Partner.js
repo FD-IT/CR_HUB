32f54965d96f18c2b1f15d80feb9e3826e93da3b
Stefan Ilic <ilicstefan272@gmail.com>
2024-03-05 21:25:50 +0100

    Dodata no sema za partnerstvo

--- models/Partner.js
+++ models/Partner.js
@@ -1,14 +1,37 @@
 const mongoose = require("mongoose");
 
 const partnerSchema = new mongoose.Schema({
-    //Dodavanje polja
-    naziv: String,
-    kontaktOsoba : String,
-    kontaktMejl : String,
-    kontaktTelefon: Number
+  //Dodavanje polja
+  naziv: String,
+  kontaktOsoba: String,
+  kontaktMejl: String,
+  kontaktTelefon: Number,
+});
 
-})
+//nova kolekcija partnerstva
+const PartnerstvoSchema = new mongoose.Schema({
+  naziv: {
+    type: String,
+    required: true,
+  },
+  tip_partnerstva: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  komentari: {
+    type: [String],
+  },
+  partner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Partner",
+  },
+});
+
+const Partnerstvo = mongoose.model("Partnerstvo", PartnerstvoSchema);
 //Eksportovanje modela
-module.exports = mongoose.model("Partner" , partnerSchema);
-
-
+module.exports = mongoose.model("Partner", partnerSchema);
+module.exports = Partnerstvo;
