86c94370b14ae61f07761de2b3ced6697d69db9f
Stefan Ilic <ilicstefan272@gmail.com>
2024-03-29 20:48:56 +0100

    Funkcija je izmenjena, dodata je konstanta saradnjePartnera, koja preko id_partera vraca sve saradnje

--- controller/partneri.js
+++ controller/partneri.js
@@ -1,4 +1,5 @@
 const Partner = require("../models/Partner");
+const Saradnje = require("../models/Saradnje");
 
 // vrati sve partnere
 const vratiSvePartnere = (req, res) => {
@@ -40,13 +41,15 @@ const obrisiPartnera = async (req, res) => {
 };
 
 // vrati partnera sa datim ID
+// *Vraca i sve saradnje u kojima se dati partner nalazi
 const vratiPartneraSaID = async (req, res, next) => {
   try {
     const partner = await Partner.findById(req.params.id);
     if (!partner) {
       return res.status(404).send("Partner nije pronadjen!");
     }
-    res.render("partner", { partner: partner });
+    const saradnjePartnera = await Saradnja.find({ partner: partner._id });
+    res.render("partner", { partner: partner, saradnje: saradnjePartnera });
   } catch (error) {
     console.error("Greska prilikom pronalazenja partnera: ", error);
     res.status(500).send("Doslo je do greske prilikom obrade zahteva");
@@ -55,18 +58,21 @@ const vratiPartneraSaID = async (req, res, next) => {
 
 // kreiraj novog partnera
 const kreirajNovogPartnera = (req, res) => {
-
   // console.log(req.body); korisceno za debuggovanje
   const partner = new Partner(req.body);
-  partner.save()
-  .then(result => {
-    res.redirect('/');
-  })
-    .catch(err => {
+  partner
+    .save()
+    .then((result) => {
+      res.redirect("/");
+    })
+    .catch((err) => {
       console.log(err);
     });
-}
+};
 
 module.exports = {
-  vratiSvePartnere, obrisiPartnera, vratiPartneraSaID, kreirajNovogPartnera,
+  vratiSvePartnere,
+  obrisiPartnera,
+  vratiPartneraSaID,
+  kreirajNovogPartnera,
 };
